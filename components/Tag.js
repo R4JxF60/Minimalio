dd7a5f10aae178a06e5549ae13182a0aa017f4ad
R4JxF60 <rajithakumaraprog@gmail.com>
2022-05-02 20:04:27 +0530

    components/Tag.js

--- components/Tag.js
+++ components/Tag.js
@@ -1,7 +1,8 @@
 function Tag(props) {
+    // +((props.tag)? props.tag.replaceAll(/[ +]/g, ""):
     return (
         <div className="mr-4 mb-4 bg-gray-400 p-2 w-fit h-fit font-lato font-normal sm:text-xl text-lg">
-            {"#"+((props.tag)? props.tag.replaceAll(/[ +]/g, ""): "tag")}
+            {"#"+props.tag || "tag"}
         </div>
     );
 }
