f7762e657e51b78a8f30f1eccc569c9c03b8e952
R4JxF60 <rajithakumaraprog@gmail.com>
2022-05-02 18:15:55 +0530

    create: Social-links component

---
+++ components/SocialLinks.js
@@ -0,0 +1,17 @@
+function SocialLinks(props) {
+    return (
+        <div className="flex items-center justify-center absolute w-full bottom-16">
+            <div className="sm:block hidden w-12 h-[1px] bg-black mr-4"></div>
+            <div className="flex items-center">
+                <object data="/images/icons/github-brands.svg"></object>
+                <object className="ml-4 mr-4" data="/images/icons/linkedin-in-brands.svg"></object>
+                <object data="/images/icons/twitter-brands.svg"></object>
+                <object className="ml-4 mr-4" data="/images/icons/instagram-brands.svg"></object>
+                <object data="/images/icons/youtube-brands.svg"></object>
+            </div>
+            <div className="sm:block hidden w-12 h-[1px] bg-black ml-4"></div>
+        </div>
+    );
+}
+
+export default SocialLinks;
