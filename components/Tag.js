db5030edf103b776f7cc1cf2fc9717eb6cd43b76
R4JxF60 <rajithakumaraprog@gmail.com>
2022-05-02 18:16:41 +0530

    create: Tag component

---
+++ components/Tag.js
@@ -0,0 +1,9 @@
+function Tag(props) {
+    return (
+        <div className="mr-4 mb-4 bg-gray-400 p-2 w-fit h-fit font-lato font-normal sm:text-xl text-lg">
+            {"#"+((props.tag)? props.tag.replaceAll(/[ +]/g, ""): "tag")}
+        </div>
+    );
+}
+
+export default Tag;
