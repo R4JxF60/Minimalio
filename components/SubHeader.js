626cd3553bbfb1d2a63dd625ec92de9113a86ab0
R4JxF60 <rajithakumaraprog@gmail.com>
2022-05-02 18:16:22 +0530

    create: Subheader component

---
+++ components/SubHeader.js
@@ -0,0 +1,9 @@
+function SubHeader(props) {
+    return (
+        <h2 className="font-merriweather font-normal mb-3 text-xl sm:text-2xl">
+            {props.children || "Sub Heading"}
+        </h2>
+    );
+}
+
+export default SubHeader;
