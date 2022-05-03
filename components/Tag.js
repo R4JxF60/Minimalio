41d9711c0b0c2e5ec5ad9a952a83ac82d01e7401
R4JxF60 <rajithakumaraprog@gmail.com>
2022-05-03 21:09:04 +0530

    create: tag component

--- components/Tag.js
+++ components/Tag.js
@@ -1,8 +1,7 @@
 function Tag(props) {
-    // +((props.tag)? props.tag.replaceAll(/[ +]/g, ""):
     return (
         <div className="mr-4 mb-4 bg-gray-400 p-2 w-fit h-fit font-lato font-normal sm:text-xl text-lg">
-            {"#"+props.tag || "tag"}
+            {"#"+((props.tag)? props.tag.replaceAll(/[ +]/g, "").toLowerCase(): "tag")}
         </div>
     );
 }
