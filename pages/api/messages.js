5644c89031224cc096aa92dec5e78369bfa6a4bb
R4JxF60 <rajithakumaraprog@gmail.com>
2022-05-06 15:08:48 +0530

    update: api endpoints

--- pages/api/messages.js
+++ pages/api/messages.js
@@ -1,5 +1,33 @@
+const nodemailer = require("nodemailer");
+
+
+
+async function mailer() {
+    let testAccount = await nodemailer.createTestAccount();
+    let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false,
+        auth: {
+            user: testAccount.user,
+            pass: testAccount.pass
+        },
+    });
+
+    let info = await transporter.sendMail({
+        from: 'rajithakumaraprog@gmail.com', // sender address
+        to: "lionrider135c@gmail.com", // list of receivers
+        subject: "Hello", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<b>Hello world?</b>", // html body
+    });
+}
+
+
+
+
 function handler(req, res) {
-    res.status(200).json({message: "your message", status: "true"});
+    res.status(200).json({request: req.body, status: "true"});
 }
 
 export default handler;
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,18 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  "plugins": [
+    "tailwindcss",
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        "autoprefixer": {
+          "flexbox": "no-2009"
+        },
+        "stage": 4,
+        "features": {
+          "custom-properties": false
+        }
+      }
+    ]
+  ]
 }
