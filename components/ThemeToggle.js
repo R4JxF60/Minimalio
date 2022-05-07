e071f521cbbfd6a6aadc7ee60922b75d87fbd2fc
R4JxF60 <rajithakumaraprog@gmail.com>
2022-05-07 21:57:52 +0530

    update: theme toggle button

--- components/ThemeToggle.js
+++ components/ThemeToggle.js
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
+
 function ThemeToggle(props) {
+
+    //useEffect(() => {})
+    
     return (
         <div>
-            
+            <img src="/images/icons/dark.svg" alt={"dark mode icon"} className="sm:ml-9 mr-6"></img>
         </div>
     );
 }
+
+export default ThemeToggle;
