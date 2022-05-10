c758c0c3d4c9ef05cc8a632bc8bd737ff60dcfb0
R4JxF60 <rajithakumaraprog@gmail.com>
2022-05-10 23:14:35 +0530

    update: home background

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
+import Background from "../components/Backgound";
 import ScrollProvider from "../context/Scroll.context";
 
 
@@ -17,10 +18,11 @@ function IndexPage() {
 
     return (
         <div className="bg-white relative">
+            <Background />
             <div className="w-full sm:h-28 sm:p-0 pl-9 pr-9 fixed h-24 z-30 bg-white">
                 <Navigation />
             </div>
-            <div className="container mx-auto pl-9 pr-9 sm:p-0 z-0" data-scroll-container>
+            <div className="container mx-auto pl-9 pr-9 sm:p-0 z-10 relative" data-scroll-container>
                 <Home />
                 <About />
                 <Projects />
