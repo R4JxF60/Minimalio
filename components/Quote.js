55d100c0db3465284c6a4c99b8b8e5da7b9172bc
R4JxF60 <rajithakumaraprog@gmail.com>
2022-05-13 02:29:54 +0530

    create: Quote component

---
+++ components/Quote.js
@@ -0,0 +1,9 @@
+function Quote() {
+    return (<section className="h-screen w-full bg-mid-black">
+        <div className="mx-auto pl-9 pr-9 sm:p-0 container w-full h-full flex justify-center items-center">
+            <div className="font-sen font-normal md:text-8xl sm:text-6xl text-5xl w-3/4 text-center text-light-gray">" I'm just a regular everyday normal guy "</div>
+        </div>
+    </section>);
+}
+
+export default Quote;
