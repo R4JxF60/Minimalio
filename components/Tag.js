ea18aaeb5fed25f0bd4b3b8ce633656eac53f53c
R4JxF60 <rajithakumaraprog@gmail.com>
2022-05-13 02:33:00 +0530

    fix: replaceAll issue

--- components/Tag.js
+++ components/Tag.js
@@ -9,7 +9,7 @@ function Tag(props) {
     return (
         <div className="mr-4 mb-4 p-2 w-fit h-fit font-lato font-normal sm:text-xl text-lg border border-solid border-mid-black text-mid-black overflow-hidden">
             <div className="tags">
-                {"#"+((props.tag)? props.tag.replaceAll(/[ +]/g, "").toLowerCase(): "tag")}
+                {"#"+((props.tag)? props.tag: "tag")}
             </div>
         </div>
     );
