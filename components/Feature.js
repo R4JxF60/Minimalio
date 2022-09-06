317481ad083228a9775fc2e7ca56020cefa0de02
R4JxF60 <rajithakumaraprog@gmail.com>
2022-09-07 02:00:37 +0530

    create: Feature holder

---
+++ components/Feature.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function Feature({children}) {
+    return(
+    <div className="flex justify-center align-middle w-full bg-mid-black">
+        {children}
+    </div>);
+};
+
+export default Feature;
