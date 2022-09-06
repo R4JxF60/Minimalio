2ba1ab6d8c686be8e51008a87deba946d5d93676
R4JxF60 <rajithakumaraprog@gmail.com>
2022-09-07 02:01:25 +0530

    update: include Feature holder

--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,12 @@
 import Home from "./Home";
 import Background from "./Backgound";
 import Navigation from "./Navigation";
+import Feature from "./Feature";
 
 function Hero(props) {
     return (<section className="relative h-screen">
         <div className="w-full sm:h-28 sm:p-0 pl-9 pr-9 fixed h-24 z-30 bg-light-gray">
+            <Feature><div>Hello from Feature!</div></Feature>
             <Navigation />
         </div>
         <Background />
