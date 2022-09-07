62596c08869beb348e3db509c36df691bd4f903f
R4JxF60 <rajithakumaraprog@gmail.com>
2022-09-07 11:20:53 +0530

    fix: margin issues

--- components/Home.js
+++ components/Home.js
@@ -7,7 +7,7 @@ function Home(props) {
 
     return (
 
-        <section className="flex flex-col justify-between h-[calc(100vh-96px)] w-full absolute text-mid-black top-24" id="home">
+        <section className="flex flex-col justify-between h-[calc(100vh-96px)] w-full absolute text-mid-black top-[136px]" id="home">
                 <div className="flex flex-col justify-center h-full w-full mx-auto container pl-9 pr-9 sm:p-0">
                     <h1 className="font-sen font-normal mb-3 sm:text-8xl text-7xl">I'm Rajitha.</h1>
                     <h2 className="font-merriweather font-normal mb-3 text-xl sm:text-2xl">Not A Developer But An Engineer.</h2>
