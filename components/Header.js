2c68dac5912df1e8c091b3f1e3df61b900d4148a
R4JxF60 <rajithakumaraprog@gmail.com>
2022-09-10 22:34:55 +0530

    fix: logging issue

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,4 @@
 function Header(props) {
-    console.log(props);
     return (
         <h1 className="font-sen font-normal sm:text-8xl text-7xl mb-3 text-mid-black">
             {props.children || "Heading"}
